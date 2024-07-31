20978c47723dda8ccee471350bc6e6f5e0022856
Ahmed <ahmed.khan@rometlimited.com>
2024-07-31 15:05:38 -0400

    pyinstaller for creating executable

--- desktop-app/release/build.sh
+++ desktop-app/release/build.sh
@@ -4,8 +4,10 @@ export PYTHONPATH="."
 # create version file
 py -3 installer/pyinstaller_version.py 
 # create executable
-py -3 -m PyInstaller -y  --console --name=jaguar_production --version-file=installer/_version scripts/jaguar_production.py --icon assets/images/logo.ico 
+# py -3 -m PyInstaller -y  --console --name=jaguar_production --version-file=release/_version scripts/jaguar_production.py --icon assets/images/logo.ico 
 
+# Ahmed's (better) way of creating executable:
+py -3 -m PyInstaller -y  --console --name=jaguar_production --version-file=release/_version scripts/jaguar_production.py --icon assets/images/icon.ico --paths=C:/Users/production/Desktop/AhmedJigTesting/desktop-app/desktop-app/.venv/Lib/site-packages --clean  --onefile
 
 #utilities scripts
 # py -3 -m PyInstaller -y  --console --name=md5sum --version-file=installer/_version scripts/md5sum.py --icon assets/images/logo.ico --onefile
