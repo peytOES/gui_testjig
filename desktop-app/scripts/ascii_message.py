3f587357c597c9f6f8bb9b1fd2bd88fb463146cc
Ahmed <ahmed.khan@rometlimited.com>
2024-07-31 15:06:48 -0400

    ascii message to match with provisioning station

---
+++ desktop-app/scripts/ascii_message.py
@@ -0,0 +1,54 @@
+FAIL_STRING = """
+
+
+
+FFFFFFFFFFFFFFFFFFFFFF      AAA               IIIIIIIIIILLLLLLLLLLL
+F::::::::::::::::::::F     A:::A              I::::::::IL:::::::::L
+F::::::::::::::::::::F    A:::::A             I::::::::IL:::::::::L
+FF::::::FFFFFFFFF::::F   A:::::::A            II::::::IILL:::::::LL
+  F:::::F       FFFFFF  A:::::::::A             I::::I    L:::::L
+  F:::::F              A:::::A:::::A            I::::I    L:::::L
+  F::::::FFFFFFFFFF   A:::::A A:::::A           I::::I    L:::::L
+  F:::::::::::::::F  A:::::A   A:::::A          I::::I    L:::::L
+  F:::::::::::::::F A:::::A     A:::::A         I::::I    L:::::L
+  F::::::FFFFFFFFFFA:::::AAAAAAAAA:::::A        I::::I    L:::::L
+  F:::::F         A:::::::::::::::::::::A       I::::I    L:::::L
+  F:::::F        A:::::AAAAAAAAAAAAA:::::A      I::::I    L:::::L         LLLLLL
+FF:::::::FF     A:::::A             A:::::A   II::::::IILL:::::::LLLLLLLLL:::::L
+F::::::::FF    A:::::A               A:::::A  I::::::::IL::::::::::::::::::::::L
+F::::::::FF   A:::::A                 A:::::A I::::::::IL::::::::::::::::::::::L
+FFFFFFFFFFF  AAAAAAA                   AAAAAAAIIIIIIIIIILLLLLLLLLLLLLLLLLLLLLLLL
+
+
+
+"""
+
+PASS_STRING = """
+
+
+
+
+
+
+
+ppppp   ppppppppp     aaaaaaaaaaaaa      ssssssssss       ssssssssss
+p::::ppp:::::::::p    a::::::::::::a   ss::::::::::s    ss::::::::::s
+p:::::::::::::::::p   aaaaaaaaa:::::ass:::::::::::::s ss:::::::::::::s
+pp::::::ppppp::::::p           a::::as::::::ssss:::::ss::::::ssss:::::s
+ p:::::p     p:::::p    aaaaaaa:::::a s:::::s  ssssss  s:::::s  ssssss
+ p:::::p     p:::::p  aa::::::::::::a   s::::::s         s::::::s
+ p:::::p     p:::::p a::::aaaa::::::a      s::::::s         s::::::s
+ p:::::p    p::::::pa::::a    a:::::assssss   s:::::s ssssss   s:::::s
+ p:::::ppppp:::::::pa::::a    a:::::as:::::ssss::::::ss:::::ssss::::::s
+ p::::::::::::::::p a:::::aaaa::::::as::::::::::::::s s::::::::::::::s
+ p::::::::::::::pp   a::::::::::aa:::as:::::::::::ss   s:::::::::::ss
+ p::::::pppppppp      aaaaaaaaaa  aaaa sssssssssss      sssssssssss
+ p:::::p
+ p:::::p
+p:::::::p
+p:::::::p
+p:::::::p
+ppppppppp
+
+
+"""
